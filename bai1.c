e8aaa2572506b55051a576e1d0de721ff6986a46
Đức Sang <32126158+blackphantom97@users.noreply.github.com>
2017-09-30 10:12:51 +0700

    bài1

---
+++ bai1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+printf( "\t\t\t\t\t\tChuc Mung!\n" );
+printf("\t\t\t\tGui den dong chi:X, Y, Z. Chuc dong chi \n\t\t\t\tda rot tot nghiep va gia nhap quan ngu.^^");
+getch();
+
+}
